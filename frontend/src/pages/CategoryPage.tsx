f63e4a21f349969228f5866b3a1622db087e6122
prathyushadantuluri <prathyusha.dantuluri@lotuscloud.me>
2026-03-10 18:42:02 -0400

    Task3 Category page

---
+++ frontend/src/pages/CategoryPage.tsx
@@ -0,0 +1,17 @@
+import CategoryGrid from "../components/category/CategoryGrid"
+
+export default function CategoryPage(){
+
+ return(
+
+  <div style={{maxWidth:"1200px",margin:"40px auto"}}>
+
+   <h1>News Categories</h1>
+
+   <CategoryGrid/>
+
+  </div>
+
+ )
+
+}
