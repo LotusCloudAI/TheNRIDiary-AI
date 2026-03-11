1a8621a514037be0514114b0dfd4d3d2c65aa09f
madhu0622 <madhushreya.vattikuti@lotuscloud.ai>
2026-03-11 23:59:36 +0530

    Task1 Community data created

---
+++ frontend/src/data/communityData.ts
@@ -0,0 +1,12 @@
+export const communityPosts = [
+ {
+  id:1,
+  title:"Dallas Telugu Association Event",
+  image:"https://images.unsplash.com/photo-1529156069898-49953e39b3ac"
+ },
+ {
+  id:2,
+  title:"Indian Cultural Festival in New Jersey",
+  image:"https://images.unsplash.com/photo-1506157786151-b8491531f063"
+ }
+]
