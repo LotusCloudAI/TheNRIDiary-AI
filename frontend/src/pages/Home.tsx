aeabf848b3cbf0b12cad9c7a045cd07844992264
neelimabollempalli-lotus <neelima.bollempalli@lotus.buzz>
2026-03-20 09:49:52 -0400

    added home page and connected routing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,93 +1,13 @@
-import HeroSection from "../components/home/HeroSection"
-import TrendingSection from "../components/home/TrendingSection"
-import StoryGridSection from "../components/home/StoryGridSection"
-import StateNewsSection from "../components/home/StateNewsSection"
-import CommunitySection from "../components/home/CommunitySection"
-import BusinessSection from "../components/home/BusinessSection"
-import ClassifiedSection from "../components/home/ClassifiedSection"
-import EventsSection from "../components/home/EventsSection"
-import VideoSection from "../components/home/VideoSection"
+import React from 'react'
 
-export default function Home() {
-
-  return (
-
-    <div
-      style={{
-        width: "100%",
-        maxWidth: "1400px",
-        margin: "0 auto",
-        padding: "30px 40px"
-      }}
-    >
-
-      {/* HERO + TRENDING */}
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "2fr 1fr",
-          gap: "40px",
-          alignItems: "start",
-          marginBottom: "50px"
-        }}
-      >
-        <HeroSection />
-        <TrendingSection />
-      </div>
-
-
-      {/* LATEST STORIES */}
-
-      <section style={{marginBottom:"50px"}}>
-        <StoryGridSection />
-      </section>
-
-
-      {/* USA STATE NEWS */}
-
-      <section style={{marginBottom:"50px"}}>
-        <StateNewsSection />
-      </section>
-
-
-      {/* COMMUNITY MODULES */}
-
-      <section style={{marginBottom:"50px"}}>
-        <CommunitySection />
-      </section>
-
-
-      {/* BUSINESS DIRECTORY + CLASSIFIEDS */}
-
-      <section
-        style={{
-          display:"grid",
-          gridTemplateColumns:"1fr 1fr",
-          gap:"40px",
-          marginBottom:"50px"
-        }}
-      >
-        <BusinessSection />
-        <ClassifiedSection />
-      </section>
-
-
-      {/* EVENTS + VIDEOS */}
-
-      <section
-        style={{
-          display:"grid",
-          gridTemplateColumns:"1fr 1fr",
-          gap:"40px",
-          marginBottom:"50px"
-        }}
-      >
-        <EventsSection />
-        <VideoSection />
-      </section>
-
+export default function Home() 
+{
+ return (
+    <div>
+        <h1>Welcome to The NRI Diary</h1>
+        <p>This is the home page.</p>
+        {/*this is the space for other developers' work */}
     </div>
+ );
 
-  )
 }
